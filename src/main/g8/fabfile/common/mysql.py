c73ba8c57937acd8e1222af647e8de37a13516bf
Hirokazu Nishioka <hirokazu.nishioka@speee.jp>
2014-12-23 23:07:26 +0900

    [ common ] mysqlを追加

---
+++ src/main/g8/fabfile/common/mysql.py
@@ -0,0 +1,39 @@
+from fabric.api import *
+from fabric.contrib.files import *
+
+@task
+def install():
+    sudo('yum -y --enablerepo=remi install mysql-server')
+
+@task
+def config():
+    upload_template(
+        'fabfile/common/templates/my.cnf',
+        '/etc/my.cnf',
+        context = {
+            'buffer_pool_size': env.MYSQL_BUFFER_POOL_SIZE
+        },
+        use_sudo = True,
+        use_jinja = True,
+        mode = 0644
+    )
+    sudo('chown root.root /etc/my.cnf')
+
+@task
+def chkconfig():
+    sudo('chkconfig mysqld on')
+
+@task
+def restart():
+    sudo('service mysqld restart')
+
+@task
+def adduser():
+    run('mysql -u root -e "grant all on *.* to \'%s\'@\'localhost\' identified by \'%s\';"' % (env.MYSQL_USER_NAME, env.MYSQL_USER_PASSWORD))
+    run('mysql -u root -e "revoke super on *.* from \'%s\'@\'localhost\';"' % env.MYSQL_USER_NAME)
+    run('mysql -u root -e "grant all on *.* to \'%s\'@\'%%\' identified by \'%s\';"' % (env.MYSQL_USER_NAME, env.MYSQL_USER_PASSWORD))
+    run('mysql -u root -e "revoke super on *.* from \'%s\'@\'%%\';"' % env.MYSQL_USER_NAME)
+
+@task
+def create_database():
+    run('mysql -u root -e "create database %s character set utf8mb4 collate utf8mb4_general_ci;"' % env.MYSQL_DATABASE_NAME)
--- src/main/g8/fabfile/env.py
+++ src/main/g8/fabfile/env.py
@@ -5,3 +5,9 @@ def default():
     env.hosts = [ '$host$' ]
     env.user = '$login_user$'
     env.key_filename = '$login_keyfile$'
+
+    # ==================== common ====================
+    env.MYSQL_BUFFER_POOL_SIZE = '$mysql_buffer_pool_size$'
+    env.MYSQL_USER_NAME = '$mysql_user_name$'
+    env.MYSQL_USER_PASSWORD = '$mysql_user_password$'
+    env.MYSQL_DATABASE_NAME = '$mysql_database_name$'
